3458a6c95d6e2d900bab6d058690c20a990c09c1
HellaxA <abereznikov64@gmail.com>
2020-11-25 12:34:26 +0200

    Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: process.env.BACKEND_API || 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080',
   invalidEmail: 'invalid.email',
   userFetched: 'user.fetched',
   resetTokenNull: 'reset.token.null',
