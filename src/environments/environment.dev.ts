c7e287a6c4df2578478a0edb7cbe16841324d969
HellaxA <abereznikov64@gmail.com>
2020-11-26 17:31:28 +0200

    Few bugs fixed

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://cheetah-test-backend-dev.herokuapp.com',
+  invalidEmail: 'invalid.email',
+  userFetched: 'user.fetched',
+  resetTokenNull: 'reset.token.null',
+  tokenExpired: 'token.expired',
+  invalidToken: 'token.invalid',
+  samePassword: 'same.password',
+  resetPasswordSuccess: 'message.resetPasswordSuc',
+  success: 'success',
+  userExists: 'User Already Exists'
+};
