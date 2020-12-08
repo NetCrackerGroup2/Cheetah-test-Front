af5af1aa672ba975c6c7a556834518c72f3dddb8
trilgar <zaritkiy.alexey@gmail.com>
2020-12-08 14:40:12 +0200

    added notifications component and connected to back

--- src/app/services/test-case-info/test-case-info.service.ts
+++ src/app/services/test-case-info/test-case-info.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActionResult} from '../../models/action-result/ActionResult';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class TestCaseInfoService {
   }
 
   getActions(idTestCase: number): Observable<ActionResult[]> {
-    const url = `http://localhost:8080/api/run-test-case/action?id=${idTestCase}`;
+    const url = `${environment.apiUrl}/api/run-test-case/action?id=${idTestCase}`;
     return this.http.get<ActionResult[]>(url);
   }
 }
