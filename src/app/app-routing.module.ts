d29c6d89fe0e0e67e354a4889822c3fd76bdf56d
Kotyara <babanina.ev@gmail.com>
2020-12-15 14:10:26 +0200

    remove LastReportDetails

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,6 @@ import {ActionsInCompoundComponent} from './components/actions-in-compound/actio
 import {HistoryTestCaseComponent} from './components/history-test-case/history-test-case.component';
 import {TestCaseListComponent} from './components/test-case/test-case-list/test-case-list.component';
 import {TestCaseConfigurationComponent} from './components/test-case/test-case-configuration/test-case-configuration.component';
-import {LastReportDetailsComponent} from './components/test-case/last-report-details/last-report-details.component';
 import {TestCaseInfoComponent} from './components/test-case-info/test-case-info.component';
 import {SendReportComponent} from './components/send-report/send-report.component';
 import {CircularDiagramComponent} from './components/circular-diagram/circular-diagram/circular-diagram.component';
@@ -120,11 +119,6 @@ const routes: Routes = [
   {
     path: 'projects/:projectId/test-cases/edit-test-case/:id',
     component: TestCaseConfigurationComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'projects/:projectId/test-cases/last-report-details',
-    component: LastReportDetailsComponent,
     canActivate: [AuthGuard]
   },
   {
